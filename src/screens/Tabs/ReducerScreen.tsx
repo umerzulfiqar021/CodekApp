2ccf3bd12e8062afadc1b683591ffb32ebb251ee
umerzulfiqar021 <umerzulfiqar38@gmail.com>
2025-01-20 10:55:05 +0500

    done with reducer

--- src/screens/Tabs/ReducerScreen.tsx
+++ src/screens/Tabs/ReducerScreen.tsx
@@ -2,12 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useReducer } from 'react'
 import Input from '../../components/Input'
 
-const reducer = (state,action)=>{
-switch(action.number){
+const reducer = (state, action)=>{
+switch(action.type){
   case 1:
-    return{...state,first:action.value}
+    return{...state, first: action.value}
     case 2:
-      return{...state,last:action.value}
+      return{...state,last: action.value}
 }
 }
 
@@ -16,13 +16,15 @@ const Reducer = () => {
   return (
     <View style = {styles.container}>
       
-      <Text>Hello! my first name is {state.first}</Text>
-      <Text>my last name is {state.last}</Text>
+      <Text>Hello! my first name is {state?.first}</Text>
+      <Text>my last name is {state?.last}</Text>
       <View>
           <Input placeholder={'Enter first name'} onSubmitEditing={(data)=> {
             dispatch({type:1,value: data})
           }}/>
-          {/* <Input placeholder={'Enter last name'} /> */}
+          <Input placeholder={'Enter last name'} onSubmitEditing={(data)=>{
+            dispatch({type:2,value:data})
+          }} />
       </View>
     </View>
 
